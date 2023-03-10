473a3b2b4cd67444d5198e916df3f3ae8b565fc6
jpadss <juanpedro_0@hotmail.com>
2023-03-09 22:35:46 -0300

    Adicionando meu projeto no github

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById('btn__mobile');
+
+btnMobile.addEventListener('click', () =>{
+
+    const menu = document.getElementById('menu-mobile');
+    menu.classList.toggle('active');
+
+})
